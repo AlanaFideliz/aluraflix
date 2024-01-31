e49dc7e211be6d43f0f4defe240c965b2c3e4f28
ALANA FIDELIZ DA SILVA <98193850+AlanaFideliz@users.noreply.github.com>
2024-01-31 16:58:46 -0300

    Add files via upload

---
+++ aluraflix/script.js
@@ -0,0 +1,15 @@
+
+
+var listaFilmes = ["https://upload.wikimedia.org/wikipedia/pt/1/1b/Schoolrockposter.jpg",
+"https://musicart.xboxlive.com/7/5dbd5100-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080",
+"https://media.fstatic.com/-gnGrQ4hd2RMq2jPb27gkZnkmWY=/322x478/smart/filters:format(webp)/media/movies/covers/2011/10/09bcdcd17d3742342058a47655fbb1ba.jpg",
+"https://br.web.img2.acsta.net/pictures/21/12/06/21/17/3973076.jpg",
+"https://cdn.awsli.com.br/2500x2500/1610/1610163/produto/208108598/poster-guardioes-da-galaxia-vol-3-a-6ec474ef.jpg",
+"https://cdn.cdn.wf/wp-content/uploads/2017/05/25.png"
+]
+
+for(var i=0; i<listaFilmes.length;i++){
+  document.write('<img src=' + listaFilmes[i]  +'>')
+  
+}
+ 
